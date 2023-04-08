e20b4ce380cacee21372f2b96cfa646b6706e68c
Moula16 <moulapallas99@gmail.com>
2023-04-08 11:57:44 +0200

    js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// confirm("are you ok")
+// var ecoin = 25
+// var note = 5
+// var mad = 16.66
+
+// console.log(ecoin)
+// console.log(typeof ecoin)
+// document.write(note)
+// document.write(typeof note)
+// alert(mad)
+
+
+var listNotes1 = [15,19,12,20,14]
+var listNotes2 = new Array[15,19,12,20,14]
+var listNotes3 = new Array()
+listNotes3[0] = 15
+listNotes3[1] = 19
+listNotes3[2] = 12
+listNotes3[3] = 20
+listNotes3[4] = 14
+var listNotes4 = []
+listNotes4[0] = 15
+listNotes4[1] = 19
+listNotes4[2] = 12
+listNotes4[3] = 20
+listNotes4[4] = 14
+console.log(listNotes4)
